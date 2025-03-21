146c6808f725a7ab125bfd2927f0f1f90868b221
dawid <dawidsurys@hotmail.com>
2025-03-21 17:03:46 +0100

    Added TreeView

--- src/models/DOType.ts
+++ src/models/DOType.ts
@@ -1,13 +1,32 @@
-export interface DOType {
+export interface LnType {
     id: string;
-    cdc: string;
-    das: DAElement[];
+    prefix: string;
+    lnClass: string;
+    inst: string;
+    dos: DoElement[] | null;
 }
 
-export interface DAElement {
+export interface DoType {
+    id: string;
+    cdc: string;
+    sdos: SdoElement[];
+    das: DaElement[];
+}
+
+export interface DoElement {
+    name: string;
+    type: DoType | null;
+}
+
+export interface DaElement {
     name: string;
     bType: string;
     valKind: string;
     val: string;
     fc: string;
+}
+
+export interface SdoElement {
+    name: string;
+    doType: DoType | null;
 }
--- src/sclReadonlyEditor.ts
+++ src/sclReadonlyEditor.ts
@@ -1,8 +1,8 @@
 
 
 import * as vscode from 'vscode';
-import {DOMParser} from 'xmldom';
-import { DOType, DAElement } from './models/DOType';
+import { DOMParser } from 'xmldom';
+import { DoType, DaElement, DoElement, SdoElement, LnType } from './models/DOType';
 
 export class SclReadonlyEditorProvider implements vscode.CustomTextEditorProvider {
 
@@ -26,7 +26,7 @@ export class SclReadonlyEditorProvider implements vscode.CustomTextEditorProvide
         webviewPanel.webview.options = {
             enableScripts: true
         };
-        webviewPanel.webview.html = this.getHtmlForWebview(document.getText());
+        webviewPanel.webview.html = this.getHtmlForWebview(webviewPanel.webview, document.getText());
 
         function updateWebview() {
             webviewPanel.webview.postMessage({
@@ -56,35 +56,52 @@ export class SclReadonlyEditorProvider implements vscode.CustomTextEditorProvide
         updateWebview();
     }
 
-    private getHtmlForWebview(content: string): string {
+    private getHtmlForWebview(webview: vscode.Webview, content: string): string {
         const parser = new DOMParser();
         const doc = parser.parseFromString(content, 'application/xml');
 
         const ied = doc.getElementsByTagName('IED')[0];
         const dataTemplate = doc.getElementsByTagName('DataTypeTemplates')[0];
+        
         const doTypes = this.getDoTypes(dataTemplate);
+        const lnTypes = this.getLnTypes(dataTemplate, doTypes);
+
+        const treeviewStyleUri = webview.asWebviewUri(vscode.Uri.joinPath(
+            this.context.extensionUri, 'media', 'treeview.css'));
+        const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(
+            this.context.extensionUri, 'media', 'style.css'));
+
+        const treeviewScriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
+            this.context.extensionUri, 'media', 'treeview.js'));
 
         return /* html */`
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
-
+            <link rel="stylesheet" href="${treeviewStyleUri}">
+            <link rel="stylesheet" href="${styleUri}">
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </head>
         <body>
-            <h1>SCL Viewer</h1>
-            <h2>IED ${ied.getAttribute('name')}</h2>
-            <h3>Logical Devices</h3>
-            <ul>
-                ${this.getLdevicesList(ied)}
-            </ul>
-            </hr>
-            <h2> Data Templates </h2>
-            <h3>Logical Node Types</h3>
-            <ul>
-                ${this.getLNodeTypeList(dataTemplate, doTypes)}
-            </ul>
+            <div class="container">
+                <div class="side-panel">
+                    <h2>IED ${ied.getAttribute('name')}</h2>
+                    <h3>Logical Devices</h3>
+                    <ul id="iedUL">
+                        ${this.getLdevicesList(ied)}
+                    </ul>
+                </div>
+                </hr>
+                <div class="side-panel">
+                    <h2> Data Templates </h2>
+                    <h3>Logical Node Types</h3>
+                    <ul id="dtUL">
+                        ${this.getLNodeTypeList(lnTypes)}
+                    </ul>
+                </div>
+            </div>
+            <script src="${treeviewScriptUri}"></script>
         </body>
         </html>`;
     }
@@ -95,15 +112,18 @@ export class SclReadonlyEditorProvider implements vscode.CustomTextEditorProvide
         // create a list of all the logical devices as <li> elements
         var lDeviceList = '';
         for (var i = 0; i < lDevices.length; i++) {
-            lDeviceList += '<li>' + lDevices[i].getAttribute('inst');
+            lDeviceList += '<li><span class="caret">' + lDevices[i].getAttribute('inst') + '</span>';
             const ln0 = lDevices[i].getElementsByTagName('LN0');
             const lNodes = lDevices[i].getElementsByTagName('LN');
 
             // create a list of all the logical nodes as nested elements of the logical device
-            lDeviceList += '<ul>';
+            lDeviceList += '<ul class="nested">';
             lDeviceList += '<li>' + ln0[0].getAttribute('lnClass') + '</li>';
             for (var j = 0; j < lNodes.length; j++) {
-                lDeviceList += `<li lnType="${lNodes[j].getAttribute('lnType')}">` + lNodes[j].getAttribute('lnClass') + '</li>';
+                const prefix = lNodes[j].getAttribute('prefix') || '';
+                const lnCLass = lNodes[j].getAttribute('lnClass') || '';
+                const inst = lNodes[j].getAttribute('inst') || '';
+                lDeviceList += `<li lnType="${lNodes[j].getAttribute('lnType')}">` + prefix + lnCLass + inst + '</li>';
             }
 
             lDeviceList += '</ul>';
@@ -113,15 +133,17 @@ export class SclReadonlyEditorProvider implements vscode.CustomTextEditorProvide
         return lDeviceList;
     }
 
-    private getDoTypes(dataTemplate: Element): DOType[] {
+    private getDoTypes(dataTemplate: Element): DoType[] {
         var doTypeNodes = dataTemplate.getElementsByTagName('DOType');
         var doTypes = [];
 
         for (var i = 0; i < doTypeNodes.length; i++) {
-            var daTags = doTypeNodes[i].getElementsByTagName('DA');
-            var das = [];
+            const daTags = doTypeNodes[i].getElementsByTagName('DA');
+            const sdoTags = doTypeNodes[i].getElementsByTagName('SDO');
+            const das = [];
+
             for (var j = 0; j < daTags.length; j++) {
-                const daElement: DAElement = {
+                const daElement: DaElement = {
                     name: daTags[j].getAttribute('name') || '',
                     bType: daTags[j].getAttribute('bType') || '',
                     valKind: daTags[j].getAttribute('valKind') || '',
@@ -131,9 +153,19 @@ export class SclReadonlyEditorProvider implements vscode.CustomTextEditorProvide
                 das.push(daElement);
             }
 
-            const doType: DOType = {
+            const sdos: SdoElement[] = [];
+            for (let j = 0; j < sdoTags.length; j++) {
+                const sdoElement: SdoElement = {
+                    name: sdoTags[j].getAttribute('name') || '',
+                    doType: doTypes.find(doType => doType.id === sdoTags[j].getAttribute('type')) || null
+                };
+                sdos.push(sdoElement);
+            }
+
+            const doType: DoType = {
                 id: doTypeNodes[i].getAttribute('id') || '',
                 cdc: doTypeNodes[i].getAttribute('cdc') || '',
+                sdos: sdos,
                 das: das
             };
 
@@ -143,51 +175,117 @@ export class SclReadonlyEditorProvider implements vscode.CustomTextEditorProvide
         return doTypes;
     }
 
-    private getLNodeTypeList(dataTemplate: Element, doTypes: DOType[]): string {
-        const lnodeTypes = dataTemplate.getElementsByTagName('LNodeType');
+    private getLnTypes(dataTemplate: Element, doTypes: DoType[]): LnType[] {
+        var lnodeTypes = dataTemplate.getElementsByTagName('LNodeType');
+        var lnTypes = [];
 
-        var lNodeTypeList = '';
         for (var i = 0; i < lnodeTypes.length; i++) {
-            lNodeTypeList += '<li>' + lnodeTypes[i].getAttribute('id');
             const doNames = lnodeTypes[i].getElementsByTagName('DO');
-            lNodeTypeList += '<ul>';
+            const dos = [];
             for (var j = 0; j < doNames.length; j++) {
                 const doType = doTypes.find(doType => doType.id === doNames[j].getAttribute('type'));
-                lNodeTypeList += '<li>DO: ' + doNames[j].getAttribute('name') + ' (' + doType?.cdc + ')';
-                lNodeTypeList += '<ul>';
-                for (var k = 0; doType && doType.das && k < doType.das.length; k++) {
-                    lNodeTypeList += '<li>' + doType?.das[k].name + ` [${doType?.das[k].fc}]` + '</li>';
-                }
+                const doElement : DoElement = {
+                    name: doNames[j].getAttribute('name') || '',
+                    type: doType || null
+                };
 
-                lNodeTypeList += '</ul>';
-                lNodeTypeList += '</li>';
+                if (doElement) {
+                    dos.push(doElement);
+                }
             }
-           
+
+            const lnType: LnType = {
+                id: lnodeTypes[i].getAttribute('id') || '',
+                prefix: lnodeTypes[i].getAttribute('prefix') || '',
+                lnClass: lnodeTypes[i].getAttribute('lnClass') || '',
+                inst: lnodeTypes[i].getAttribute('inst') || '',
+                dos: dos
+            };
+
+            lnTypes.push(lnType);
+        }
+
+        return lnTypes;
+    }
+
+    private getLNodeTypeList(lnodeTypes: LnType[]): string {
+        var lNodeTypeList = '';
+        for (var i = 0; i < lnodeTypes.length; i++) {
+            lNodeTypeList += '<li><span class="caret">' + lnodeTypes[i].lnClass + ' (' + lnodeTypes[i].id + ')</span>';
+            const doElements = lnodeTypes[i].dos;
+            
+            lNodeTypeList += '<ul class="nested">';
+            lNodeTypeList += this.getDosList(doElements);
             lNodeTypeList += '</ul>';
+            
             lNodeTypeList += '</li>';
         }
 
         return lNodeTypeList;
     }
 
-    private handleDeleteMessage(document: vscode.TextDocument, text: any) {
-        return this.updateTextDocument(document, text);
+    private getDosList(doNames: DoElement[] | null) {
+        let dosList: string = '';
+        for (var j = 0; doNames && j < doNames.length; j++) {
+            const doType = doNames[j].type;
+            dosList += '<li><span class="caret">DO: ' + doNames[j].name + ' (' + doType?.cdc + ')</span>';
+            dosList += '<ul class="nested">';
+
+            if(doType?.sdos) {
+                dosList += this.getSDosList(doType?.sdos);
+            }
+
+            for (var k = 0; doType && doType.das && k < doType.das.length; k++) {
+                dosList += '<li>' + doType?.das[k].name + ` [${doType?.das[k].fc}]` + '</li>';
+            }
+
+            dosList += '</ul>';
+            dosList += '</li>';
+        }
+        return dosList;
+    }
+
+    private getSDosList(doNames: SdoElement[]) {
+        let sdosList: string = '';
+        for (var j = 0; doNames && j < doNames.length; j++) {
+            const doType = doNames[j].doType;
+            sdosList += '<li><span class="caret">SDO: ' + doNames[j].name + ' (' + doType?.cdc + ')</span>';
+            sdosList += '<ul class="nested">';
+
+            for (let k = 0; doType && doType.sdos && k < doType.sdos.length; k++) {
+                sdosList += this.getSDosList(doType?.sdos);
+            }
+
+            for (var k = 0; doType && doType.das && k < doType.das.length; k++) {
+                sdosList += '<li>' + doType?.das[k].name + ` [${doType?.das[k].fc}]` + '</li>';
+            }
+
+            sdosList += '</ul>';
+            sdosList += '</li>';
+        }
+        return sdosList;
+    }
+
+    private handleDeleteMessage(document: vscode.TextDocument, content: any) {
+        return this.updateTextDocument(document, content);
     }
 
     private handleAddMessage(document: vscode.TextDocument) {
         return this.updateTextDocument(document, document.getText());
     }
 
-	private updateTextDocument(document: vscode.TextDocument, json: string) {
-		const edit = new vscode.WorkspaceEdit();
+    private updateTextDocument(document: vscode.TextDocument, json: string) {
+        const edit = new vscode.WorkspaceEdit();
 
-		// Just replace the entire document every time for this example extension.
-		// A more complete extension should compute minimal edits instead.
-		edit.replace(
-			document.uri,
-			new vscode.Range(0, 0, document.lineCount, 0),
-			json);
+        // Just replace the entire document every time for this example extension.
+        // A more complete extension should compute minimal edits instead.
+        let resource = edit.get(document.uri);
 
-		return vscode.workspace.applyEdit(edit);
-	}
+        edit.replace(
+            document.uri,
+            new vscode.Range(0, 0, document.lineCount, 0),
+            json);
+
+        return vscode.workspace.applyEdit(edit);
+    }
 }
